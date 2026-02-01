08c2b75a281b0c35d63cd057066f96523d923d30
Suhel Khan <suhelkhan@gmail.com>
2026-02-01 03:30:56 +0000

    added

--- app.js
+++ app.js
@@ -54,7 +54,8 @@ const html = `
   <div class="card">
     <h1> Node.js Docker App</h1>
     <p>Your application is running successfully inside a Docker container.</p>
-    <div class="badge">Docker Container Running</div>
+    <div class="badge">Docker Container Running on Top</div>
+    <div class="badge">Thanks</div>
     <p style="margin-top: 25px; font-size: 14px;">
       Port: ${PORT}
     </p>
